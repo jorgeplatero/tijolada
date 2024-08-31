3d35ebb246b600cae4c81ed2101c645a6f85d2de
jorgeplatero <jorge.platero@outlook.com>
2024-08-30 21:04:26 -0300

    atualizando arquivos

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -24,10 +24,17 @@ colunas_venda = [
     'ID Venda', 'Data', 'ID Cliente', 'Endereço de Entrega', 'Bairro de Entrega', 'Observações', 
     'Faturamento (R$)', 'Situação do Pagamento', 'Situação da Entrega', 'Forma de Pagamento'
 ]
+colunas_itens_venda = ['ID Item Compra', 'ID Venda', 'ID Produto', 'Preço Unitário', 'Quantidade']
+colunas_compras = [
+    'ID Compra', 'Data', 'ID Fornecedor', 'Custo (R$)', 'Situação do Pagamento', 
+    'Situação da Entrega', 'Forma de Pagamento'
+]
+colunas_itens_compra = ['ID Item Compra', 'ID Compra', 'ID Produto', 'Preço Unitário', 'Quantidade']
 colunas_cliente = [
     'ID Cliente', 'Nome', 'Tipo', 'CPF/CNPJ', 'Endereço', 'Bairro', 'Telefone', 
     'Referência', 'Situação'
 ]
+colunas_fornecedor = ['ID Fornecedor', 'Nome', 'CNPJ', 'Endereço', 'Bairro', 'Telefone']
 meses = {
         'January': 'Janeiro',
         'February': 'Fevereiro',
@@ -55,6 +62,94 @@ with col2:
 tab1, tab2, tab3 = st.tabs(['Compras', 'Vendas', 'Estoque'])
 
 with tab1:
+    st.subheader('Indicadores de Compras 🛒')
+    st.write('')
+    #seletor data
+    col1, _ = st.columns([.2, .8])
+    with col1:
+        data = st.date_input(
+            key='seletor_data_compra',
+            label='**Data**',
+            help='Escolha uma data para análise'
+        )
+    st.write('')
+    #dataframes
+    #---------------------------------------------------------------
+    #vendas no ano selecionado
+    df_compras = pd.DataFrame(utils.consulta_compras(), columns=colunas_compras)
+    df_compras = df_compras[df_compras.Data.dt.year == data.year]
+    #clientes no ano selecionado
+    df_fornecedor =  pd.DataFrame(utils.consulta_fornecedores(), columns=colunas_fornecedor)
+    df_fornecedor = pd.merge(df_compras, df_fornecedor, how='outer', on='ID Fornecedor')
+    #cards
+    #---------------------------------------------------------------
+    total_custo_dia = formata_valor(df_compras[(df_compras.Data.dt.date == data) & (df_compras['Situação do Pagamento'] == 'Realizado')]['Custo (R$)'].astype('float').sum())
+    total_custo_mes = formata_valor(df_compras[(df_compras.Data.dt.month == data.month) & (df_compras['Situação do Pagamento'] == 'Realizado')]['Custo (R$)'].astype('float').sum())
+    total_custo_ano = formata_valor(df_compras[df_compras['Situação do Pagamento'] == 'Realizado']['Custo (R$)'].astype('float').sum())
+
+    #figuras
+    #---------------------------------------------------------------
+    #faturamento mensal
+    df_compras_custo_mensal = df_compras[df_compras['Situação do Pagamento'] == 'Realizado']
+    df_compras_custo_mensal['Mês'] = df_compras_custo_mensal['Data'].dt.to_period('M')
+    df_compras_custo_mensal = df_compras_custo_mensal.groupby('Mês')['Custo (R$)'].sum().reset_index()
+    df_compras_custo_mensal['Mês'] = df_compras_custo_mensal['Mês'].dt.strftime('%B')
+    df_compras_custo_mensal['Mês'] = df_compras_custo_mensal['Mês'].map(meses)
+    fig_custo_mensal = px.line(
+        data_frame=df_compras_custo_mensal, 
+        x='Mês', 
+        y='Custo (R$)',
+        text=df_compras_custo_mensal['Custo (R$)'].apply(lambda x: f'R$ {x:.2f}'),
+        hover_name='Mês'
+    )
+    fig_custo_mensal.update_layout(
+        title=f'Custo Mensal em {data.year}',
+        xaxis_title='Mês', 
+        yaxis_showticklabels=False
+    )
+    fig_custo_mensal.update_traces(
+        textposition='top center',
+        hovertemplate='Mês: %{hovertext}<br>Custo: R$ %{y:.2f}'
+    )
+
+    #faturamento por cliente
+    df_custo_por_fornecedor = df_fornecedor[df_fornecedor['Situação do Pagamento'] == 'Realizado']
+    df_custo_por_fornecedor = df_custo_por_fornecedor.groupby('Nome')['Custo (R$)'].sum().reset_index()
+    df_custo_por_fornecedor = df_custo_por_fornecedor.sort_values('Custo (R$)', ascending=False).head()
+    df_custo_por_fornecedor['Custo (R$)'] = df_custo_por_fornecedor['Custo (R$)'].astype('float64')
+    fig_custo_fornecedor = px.bar(
+        data_frame=df_custo_por_fornecedor, 
+        x='Nome', 
+        y='Custo (R$)',
+        text=df_custo_por_fornecedor['Custo (R$)'].apply(lambda x: f'R$ {x:.2f}'),
+        color='Custo (R$)',
+        hover_name='Nome'
+    )
+    fig_custo_fornecedor.update_layout(
+        title=f'Top 5 Clientes em {data.year}',
+        xaxis_type='category',
+        yaxis_showticklabels=False
+    )
+    fig_custo_fornecedor.update_traces(
+        showlegend=False,
+        textposition='outside',
+        hovertemplate='Nome: %{hovertext}<br>Custo: R$ %{y:.2f}'
+    )
+    fig_custo_fornecedor.update_xaxes(
+        tickangle=45
+    )
+
+    #dashboard
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.metric(f'**Custo total em {data.day}/{data.month}/{data.year}**', f'R$ {total_custo_dia}')
+    with col2:
+        st.metric(f'**Custo total em {data.month}/{data.year}**', f'R$ {total_custo_mes}')
+    with col3:
+        st.metric(f'**Custo total em {data.year}**', f'R$ {total_custo_ano}')
+    #graficos
+    st.plotly_chart(fig_custo_mensal)
+    st.plotly_chart(fig_custo_fornecedor)
     st.warning('Em desenvolvimento')
 
 with tab2:
@@ -64,14 +159,19 @@ with tab2:
     col1, _ = st.columns([.2, .8])
     with col1:
         data = st.date_input(
+            key='seletor_data_venda',
             label='**Data**',
             help='Escolha uma data para análise'
         )
     st.write('')
     #dataframes
+    #---------------------------------------------------------------
+    #vendas no ano selecionado
     df_vendas = pd.DataFrame(utils.consulta_vendas(), columns=colunas_venda)
     df_vendas = df_vendas[df_vendas.Data.dt.year == data.year]
+    #clientes no ano selecionado
     df_clientes =  pd.DataFrame(utils.consulta_clientes(), columns=colunas_cliente)
+    df_clientes = pd.merge(df_vendas, df_clientes, how='outer', on='ID Cliente')
     #cards
     #---------------------------------------------------------------
     total_faturado_dia = formata_valor(df_vendas[(df_vendas.Data.dt.date == data) & (df_vendas['Situação do Pagamento'] == 'Realizado')]['Faturamento (R$)'].astype('float').sum())
@@ -104,24 +204,22 @@ with tab2:
     )
 
     #faturamento por cliente
-    df_vendas_faturamento_cliente = pd.merge(df_vendas, df_clientes, how='outer', on='ID Cliente')
-    df_vendas_faturamento_cliente = df_vendas_faturamento_cliente[df_vendas_faturamento_cliente['Situação do Pagamento'] == 'Realizado']
-    df_vendas_faturamento_cliente = df_vendas_faturamento_cliente.groupby('Nome')['Faturamento (R$)'].sum().reset_index()
-    df_vendas_faturamento_cliente = df_vendas_faturamento_cliente.sort_values('Faturamento (R$)', ascending=False).head()
-    df_vendas_faturamento_cliente['Faturamento (R$)'] = df_vendas_faturamento_cliente['Faturamento (R$)'].astype('float64')
+    df_faturamento_por_cliente = df_clientes[df_clientes['Situação do Pagamento'] == 'Realizado']
+    df_faturamento_por_cliente = df_faturamento_por_cliente.groupby('Nome')['Faturamento (R$)'].sum().reset_index()
+    df_faturamento_por_cliente = df_faturamento_por_cliente.sort_values('Faturamento (R$)', ascending=False).head()
+    df_faturamento_por_cliente['Faturamento (R$)'] = df_faturamento_por_cliente['Faturamento (R$)'].astype('float64')
     fig_faturamento_cliente = px.bar(
-        data_frame=df_vendas_faturamento_cliente, 
+        data_frame=df_faturamento_por_cliente, 
         x='Nome', 
         y='Faturamento (R$)',
-        text=df_vendas_faturamento_cliente['Faturamento (R$)'].apply(lambda x: f'R$ {x:.2f}'),
-        hover_name='Nome',
-        color='Faturamento (R$)'
+        text=df_faturamento_por_cliente['Faturamento (R$)'].apply(lambda x: f'R$ {x:.2f}'),
+        color='Faturamento (R$)',
+        hover_name='Nome'
     )
     fig_faturamento_cliente.update_layout(
         title=f'Top 5 Clientes em {data.year}',
         xaxis_type='category',
-        yaxis_showticklabels=False,
-        width=800
+        yaxis_showticklabels=False
     )
     fig_faturamento_cliente.update_traces(
         showlegend=False,
@@ -135,14 +233,15 @@ with tab2:
     #dashboard
     col1, col2, col3 = st.columns(3)
     with col1:
-        st.metric(f'**Total faturado no dia {data.day} do {data.month} de {data.year}**', f'R$ {total_faturado_dia}')
+        st.metric(f'**Total faturado em {data.day}/{data.month}/{data.year}**', f'R$ {total_faturado_dia}')
     with col2:
-        st.metric(f'**Total faturado no mês {data.month}**', f'R$ {total_faturado_mes}')
+        st.metric(f'**Total faturado em {data.month}/{data.year}**', f'R$ {total_faturado_mes}')
     with col3:
-        st.metric(f'**Total faturado no ano de {data.year}**', f'R$ {total_faturado_ano}')
+        st.metric(f'**Total faturado em {data.year}**', f'R$ {total_faturado_ano}')
     #graficos
     st.plotly_chart(fig_faturamento_mensal)
     st.plotly_chart(fig_faturamento_cliente)
     
 with tab3:
+    st.subheader('Estoque 📦')
     st.warning('Em desenvolvimento')
